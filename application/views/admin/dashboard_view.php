e0156f902499d1bd2485976df9010ee9be37380f
DarkPotatoKing <kyle.rosales97@gmail.com>
2017-05-25 09:27:17 +0800

    Add active users in view

--- application/views/admin/dashboard_view.php
+++ application/views/admin/dashboard_view.php
@@ -4,12 +4,42 @@
     echo "Username: ";
     echo $user->username."<br>";
     echo "Name: ";
-    echo $user->first_name." "; 
+    echo $user->first_name." ";
     echo $user->last_name."<br>";
     echo "Address: ";
     echo $user->address."<br>";
     echo "Birthday: ";
     echo $user->birthday."<br><br>";
 
+    echo "Active users:<br>";
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "db_153";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT user_id FROM active_logins";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            if ($result2 = $conn->query("SELECT username FROM users WHERE id=" . $row["user_id"]))
+            {
+                echo $result2->fetch_assoc()["username"];
+                echo "<br>";
+            }
+        }
+    } else {
+        echo "0 results";
+    }
+    $conn->close();
   ?>
 </div>
